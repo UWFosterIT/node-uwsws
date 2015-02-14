86d5c62ae75a523573d5fe2b85cd9c830e56bc9a
Nogbit <milesm@uw.edu>
2015-02-14 00:54:24 -0800

    eslint cleanup and use of chai should vs. expect

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,31 @@
 
 module.exports = function(grunt) {
 
-  // Add the grunt-mocha-test tasks.
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-eslint');
 
   grunt.initConfig({
-    // Configure a mochaTest task
+
     mochaTest: {
       test: {
         options: {
-          reporter: 'spec',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+          reporter:    'spec',
+          captureFile: 'results.txt',
+          quiet:       false
         },
-        src: ['test/**/*.js']
+        src:
+          ['test/**/*.js']
       }
+    },
+
+    eslint: {
+      options: {
+        configFile: '.eslintrc'
+      },
+      target:
+        ['*.js', 'test/**/*.js']
     }
   });
 
-  grunt.registerTask('default', 'mochaTest');
-
+  grunt.registerTask('default', ['mochaTest', 'eslint']);
 };
--- test/unit/term.js
+++ test/unit/term.js
@@ -1,22 +1,24 @@
 'use strict';
 
-var assert = require("assert")
+var chai = require('chai');
+chai.should();
+
 describe('Term', function(){
   describe('Current', function(){
     it('should return one when it is known to exist', function(){
-      assert.equal(-1, [1,2,3].indexOf(1));
-    })
-  })
+      true.should.equal(false);
+    });
+  });
 
   describe('Next', function(){
     it('should return one when it is known to exist', function(){
-      assert.equal(-1, [1,2,3].indexOf(1));
-    })
-  })
+      true.should.equal(false);
+    });
+  });
 
   describe('Previous', function(){
     it('should return one when it is known to exist', function(){
-      assert.equal(-1, [1,2,3].indexOf(1));
-    })
-  })
-})
+      true.should.equal(false);
+    });
+  });
+});
