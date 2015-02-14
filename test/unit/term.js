848131102af0487532dc8e2a91a21e2c09f39822
Nogbit <milesm@uw.edu>
2015-02-13 16:41:31 -0800

    first commit TG-9 #in-progress

---
+++ test/unit/term.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var assert = require("assert")
+describe('Term', function(){
+  describe('Current', function(){
+    it('should return one when it is known to exist', function(){
+      assert.equal(-1, [1,2,3].indexOf(1));
+    })
+  })
+
+  describe('Next', function(){
+    it('should return one when it is known to exist', function(){
+      assert.equal(-1, [1,2,3].indexOf(1));
+    })
+  })
+
+  describe('Previous', function(){
+    it('should return one when it is known to exist', function(){
+      assert.equal(-1, [1,2,3].indexOf(1));
+    })
+  })
+})
