b95358d06373aba979efd76bbcd86aa5eab567a6
Nogbit <milesm@uw.edu>
2015-03-12 12:12:41 -0700

    adding missing params to program search

--- src/modules/program.js
+++ src/modules/program.js
@@ -23,10 +23,12 @@ class Program extends Service {
 
   search (opt, cb) {
     let params = {
-      major_abbr: opt.major   || '',
-      pathway:    opt.pathway || 0,
-      quarter:    opt.quarter || '',
-      year:       opt.year    || '',
+      first_effective_quarter: opt.quarter || '',
+      first_effective_year:    opt.year    || '',
+      major_abbr:              opt.major   || '',
+      pathway:                 opt.pathway || 0,
+      quarter:                 opt.quarter || '',
+      year:                    opt.year    || ''
     };
 
     let query = qs.stringify(params);
