7ff2919c34b36e3c6bab0628093dde76b678225b
Kevin Coleman <gabugabu@uw.edu>
2016-03-18 12:20:39 -0700

    refactoring to use promises WIP; person completed

--- test/unit/person-test.js
+++ test/unit/person-test.js
@@ -7,27 +7,27 @@ describe('Person', function () {
   });
 
   describe('Get', () => {
-    it('should return ont that matches the query', (done) => {
+    it('should return ont that matches the query', () => {
       let regid = '9136CCB8F66711D5BE060004AC494FFE';
 
-      uwsws.person.get(regid, (err, response, person) => {
-        expect(person.UWNetID).to.equal('javerage');
-        done(err);
-      });
+      return uwsws.person.get(regid)
+        .then((result) => {
+          expect(result.person.UWNetID).to.equal('javerage');
+        });
     });
   });
 
   describe('Search', () => {
-    it('should return some that match the query', (done) => {
+    it('should return some that match the query', () => {
       let options = {
         netid: 'javerage'
       };
 
-      uwsws.person.search(options, (err, response, result) => {
-        expect(result.Persons).to.have.length.above(0);
-        expect(result.Persons[0].UWNetID).to.equal('javerage');
-        done(err);
-      });
+      return uwsws.person.search(options)
+        .then((result) => {
+          expect(result.person.Persons).to.have.length.above(0);
+          expect(result.person.Persons[0].UWNetID).to.equal('javerage');
+        });
     });
   });
 });
