6437f6f05cdc14daf2f961632e6659783d3d32db
Kevin Coleman <gabugabu@uw.edu>
2016-03-18 11:51:14 -0700

    refactoring to use promises WIP; course completed

--- src/modules/course.js
+++ src/modules/course.js
@@ -6,32 +6,41 @@ class Course extends Service {
     super(config);
   }
 
-  get(opt, cb) {
+  get(opt) {
     let query = `${opt.year},${opt.quarter},${opt.curriculum},${opt.course}`;
-    this._get(`course/${query}.json`, cb);
-    return;
+
+    return this._get(`course/${query}.json`)
+      .then((result) => {
+        result.course = result.data;
+        delete result.data;
+        return result;
+      });
   }
 
-  search(opt, cb) {
+  search(opt) {
     let params = {
-      changed_since_date:             opt.changeDate     || '',
-      course_number:                  opt.course         || '',
-      course_title_contains:          opt.contains       || '',
-      course_title_starts:            opt.startsWith     || '',
-      curriculum_abbreviation:        opt.curriculum     || '',
-      exlude_coures_without_sections: opt.exclude        || '',
-      future_terms:                   opt.future         || 0,
-      page_size:                      opt.size           || '',
-      page_start:                     opt.start          || '',
+      changed_since_date:             opt.changeDate || '',
+      course_number:                  opt.course || '',
+      course_title_contains:          opt.contains || '',
+      course_title_starts:            opt.startsWith || '',
+      curriculum_abbreviation:        opt.curriculum || '',
+      exlude_coures_without_sections: opt.exclude || '',
+      future_terms:                   opt.future || 0,
+      page_size:                      opt.size || '',
+      page_start:                     opt.start || '',
       transcriptable_course:          opt.transcriptable || '',
-      quarter:                        opt.quarter        || '',
-      year:                           opt.year           || ''
+      quarter:                        opt.quarter || '',
+      year:                           opt.year || ''
     };
 
     let query = qs.stringify(params);
 
-    this._get(`course.json?${query}`, cb);
-    return;
+    return this._get(`course.json?${query}`)
+      .then((result) => {
+        result.course = result.data;
+        delete result.data;
+        return result;
+      });
   }
 }
 
